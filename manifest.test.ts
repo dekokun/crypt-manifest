281a1ca9d17ab115d331c956c259ec7f87b51c10
dekokun <dekokun@hatena.ne.jp>
2022-01-17 16:28:54 +0900

    helm dep update

--- manifest.test.ts
+++ manifest.test.ts
@@ -4,12 +4,14 @@ import * as fs from "fs";
 
 const execPromise = util.promisify(child_process.exec);
 
+jest.setTimeout(10 * 1000)
 describe('snapshot test for k8s manifest', () =>{
     const baseDir = "./charts"
     const chartDirs = fs.readdirSync(`${baseDir}`);
     chartDirs.map(systemDir => {
         const target = `${baseDir}/${systemDir}`
         test(target, async () => {
+            await execPromise(`helm dep update ${target}`);
           const {stdout} = await execPromise(`helm template ${target}`);
           expect(stdout).toMatchSnapshot();
         });
